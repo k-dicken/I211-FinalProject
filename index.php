85f1edb1235892ec88ddc1b17fdb1f0d717aae0d
k-dicken <78804757+k-dicken@users.noreply.github.com>
2022-03-30 19:06:22 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require_once ("www/includes/header.php");
+
+?>
+
+<div id="hero"></div>
+
+<?php
+require_once ("www/includes/footer.php");
---
+++ www/includes/footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <a href="index.php"><img src="" alt="" class="logo"></a>
+    <p class="footer-address">12345 Skies Lane San Francisco, California 67890</p>
+    <p class="footer-phone">(901) 123-4567</p>
+    <br>
+    <p class="footer-credit">Project by<br>Kylee Dicken & Oluwatishe Elesinnla</p>
+    <br>
+    <p class="footer-copyright">Copyright Skyline 2022</p>
+</footer>
+</body>
+</html>
